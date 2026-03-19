97786c18eb895c38926e7a125e4ca193c566be94
Udaya Murthy <udayamurthy.pro@gmail.com>
2026-03-19 23:01:12 +0530

    Implement Singleton pattern and add NoteDao to NoteDb

--- app/src/main/java/com/uday/notesapp/roomdb/NoteDb.kt
+++ app/src/main/java/com/uday/notesapp/roomdb/NoteDb.kt
@@ -1,14 +1,32 @@
 package com.uday.notesapp.roomdb
 
+import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
+import androidx.room.RoomDatabase
 
 @Database(entities = [Note::class], version = 1, exportSchema = false)
-abstract class NoteDb {
+abstract class NoteDb : RoomDatabase() {
 
-    var instance = Room.databaseBuilder(
-        context = context.applicationContext,
-        NoteDb::class.java,
-        "notes_database").build()
+    abstract var noteDao : NoteDao
 
+    companion object {
+        @Volatile
+        private var INSTANCE : NoteDb? = null
+
+        fun getInstance(context: Context) : NoteDb {
+            synchronized(this) {
+                var instance = INSTANCE
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context = context,
+                        NoteDb::class.java,
+                        "notes_database"
+                    ).build()
+                }
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
 }
